df200b28d362c9c61c652097c906c81ea8428b29
Wei <chef27615@gmail.com>
2019-04-04 16:18:58 -0400

    switched to styled componenet

--- instagram/instagram/src/component/Header.js
+++ instagram/instagram/src/component/Header.js
@@ -1,4 +1,43 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'; 
+
+
+const HeaderDiv = styled.div`
+  width: 55%;
+  height: 3rem;;
+  margin: 20px auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+`
+const SearchBar = styled.div`
+  align-self: center;
+  height: 100%;
+
+  input{
+    margin-top: 13px;
+    width: 100%;
+    text-align: center;
+    height: 25px;
+    background-color: #ededed;
+    border: 1px solid #dcdcdc;
+    border-radius: 5px;
+  }
+
+`
+const UserIcon = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+
+  i{
+    font-size: 25px;
+    margin-top: 13px;
+    color: #999;
+  }
+
+`
+
+
 
 class Header extends Component {
   
@@ -9,29 +48,29 @@ class Header extends Component {
   
   
   render() {
-    console.log('in Header comp: ', this.state.user)
+    
     return (
-      <div className="headerDiv">
+      <HeaderDiv>
         <div className="instaLogos">
             <i className="fab fa-instagram">  |</i>
             <h2 style={logoFont}> Instagram</h2>
         </div>
          
-        <div className="searchBar">
+        <SearchBar>
             <input 
                 type="text"
                 title=""
                 placeholder="Search"
                 onChange={this.handlechange}
             />
-        </div>
+        </SearchBar>
 
-        <div className="userIcon">
+        <UserIcon>
             <i className="far fa-compass"></i>
             <i className="far fa-heart"></i>
             <i className="far fa-user"></i>
-        </div>
-      </div>
+        </UserIcon>
+      </HeaderDiv>
     )
   }
 }
--- instagram/instagram/src/component/PostContainer.js
+++ instagram/instagram/src/component/PostContainer.js
@@ -2,6 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CommentSection from './CommentSection';
 import PostIcon from './PostIcon'
+import styled from 'styled-components';
+
+const PostWrapper = styled.div`
+    border: 1px solid #dcdcdc;
+    width: 54.5%;
+    margin: 10px auto;
+    
+    .userInfo{
+        margin: 0 auto;
+        display: flex;
+        
+        img{
+            height: 40px;
+            margin: 10px 20px;
+            border-radius: 50%;
+        }
+    }
+
+    .postTime{
+        text-align: start;
+        margin-top: 5px;
+        margin-left: 20px;
+        margin-bottom: 20px;
+        font-size: 11px;
+        color: #555;
+    }
+
+
+`
+
+
+
 
 class PostContainer extends React.Component{
 
@@ -13,8 +45,7 @@ class PostContainer extends React.Component{
     const {username, thumbnailUrl, imageUrl, likes, timestamp, comments } = this.props.user
     //console.log(comments)
         return(
-            <div className="postContainerWrapper">
-                <div className="postWrapper">
+                <PostWrapper>
                     <div className="userInfo">
                     <img src={thumbnailUrl} alt="userThumbNail" />
                     <h3><strong>{username}</strong></h3>
@@ -25,9 +56,7 @@ class PostContainer extends React.Component{
                     </div>
                     <CommentSection comments = {comments} key = {comments.id}/>
                      <div className="postTime">{timestamp}</div>
-
-                </div>
-            </div>
+                </PostWrapper>
         );
     } 
 
